3cd5498b2486b7a5e8377f68be79b782e4f5ef53
Md. Shohidujjaman Sajib <sajibwebdev@gmail.com>
2023-09-29 12:33:01 +0600

    setup: project sections setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,31 @@
 import "./app.css";
-
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import {
+  AboutUs,
+  Hero,
+  NewsLetter,
+  Offers,
+  Products,
+  Reviews,
+} from "./sections";
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <nav>
+        <Navbar />
+      </nav>
+      <main>
+        <Hero />
+        <Products />
+        <AboutUs />
+        <Offers />
+        <Reviews />
+        <NewsLetter />
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </>
+  );
 }
---
+++ src/sections/index.js
@@ -0,0 +1,9 @@
+import AboutUs from "./AboutUs";
+import ContactUs from "./ContactUs";
+import Hero from "./Hero";
+import NewsLetter from "./NewsLetter";
+import Offers from "./Offers";
+import Products from "./Products";
+import Reviews from "./Reviews";
+
+export { AboutUs, ContactUs, Hero, NewsLetter, Offers, Products, Reviews };
